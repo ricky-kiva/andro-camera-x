a6be2ee3e1a6ff60e7482c07c1f178877653432d
ricky-kiva <ricky.kiva@gmail.com>
2023-04-29 17:47:25 +0700

    add Intent Camera

--- app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
@@ -9,15 +9,19 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var currentPhotoPath: String
 
     // launcher for CameraX
     private val launcherIntentCameraX = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
@@ -43,8 +47,15 @@ class MainActivity : AppCompatActivity() {
     // launcher for default Camera (Intent Camera)
     private val launcherIntentCamera = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         if (it.resultCode == RESULT_OK) {
+            /* Not using this anymore, because we use Full Intent Camera (not thumbnail)
             val imageBitmap = it.data?.extras?.get("data") as Bitmap
-            binding.previewImageView.setImageBitmap(imageBitmap)
+            binding.previewImageView.setImageBitmap(imageBitmap)*/
+
+            val myFile = File(currentPhotoPath)
+            myFile.let { file ->
+                // set the View
+                binding.previewImageView.setImageBitmap(BitmapFactory.decodeFile(file.path))
+            }
         }
     }
 
@@ -74,7 +85,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun startTakePhoto() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        launcherIntentCamera.launch(intent)
+        // checks whether there is activity that can handle given intent (IMAGE_CAPTURE)
+        // packageManager is Android service that give information about installed app
+        intent.resolveActivity(packageManager)
+
+        createTempFile(application).also {
+            // create Uri for the temporary file
+            val photoURI: Uri = FileProvider.getUriForFile(this@MainActivity, "com.dicoding.picodiploma.mycamera", it)
+            // get absolute path of the photoUri
+            currentPhotoPath = it.absolutePath
+            // set EXTRA_OUTPUT to the Uri of the temporary file
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            // launch the intent via launcherIntentCamera variable
+            launcherIntentCamera.launch(intent)
+        }
     }
 
     private fun startCameraX() {
